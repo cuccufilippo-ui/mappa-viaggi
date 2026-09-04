f572f7ae0661062640c35d347bc47bf33633550f
cuccufilippo-ui <cuccu.filippo@gmail.com>
2026-09-04 16:43:18 +0200

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE="mappa-viaggi-v1";
+const CORE=["/mappa-viaggi/","/mappa-viaggi/index.html","/mappa-viaggi/manifest.webmanifest","/mappa-viaggi/icon.svg"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match("/mappa-viaggi/"))));
+});
